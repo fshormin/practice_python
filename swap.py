c4c5236ad2db1fdc43964dc5658313f110bd786e
fshormin <83106829+fshormin@users.noreply.github.com>
2021-09-27 17:20:16 -0500

    Added swap.py file

---
+++ swap.py
@@ -0,0 +1,16 @@
+# Swap two variables.
+
+a = int(input("Give me var_1:"))
+b = int(input("Give me var_2:"))
+
+# print("a, b = ", a, b)
+
+# temp_var= a
+# a=b
+# b=temp_var
+
+# print(a,b)
+
+a,b = b,a
+print(a,b)
+
