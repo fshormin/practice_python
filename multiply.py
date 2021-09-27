e8b710fb14529c19ff8171b7ea5b269fc0cb66aa
fshormin <83106829+fshormin@users.noreply.github.com>
2021-09-27 17:24:37 -0500

    Added multiply.py file

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Take two inputs from the user. One will be an integer. 
+# The other will be a float number. 
+# Then multiply them to display the output.
+
+int_num= int(input("Give me an integer: "))
+float_num = float(input("Give me a float: "))
+resut = int_num * float_num
+print(resut)
